17a0f4d70fecbfded9bad2670bb3230e13985ec9
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-17 22:52:13 +0200

    AddApplicationDbContext + CreateDB

---
+++ IhorsBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IhorsBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IhorsBookWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
