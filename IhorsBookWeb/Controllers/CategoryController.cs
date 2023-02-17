4cbb504d7799f541805e5546ad2af763a1c555b1
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-18 00:19:26 +0200

    Controller,Retrieve,Display all Categories

---
+++ IhorsBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using IhorsBookWeb.Data;
+using IhorsBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IhorsBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
