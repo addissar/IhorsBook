22cf3c0dce816aa4f288b82b3466924fcb361208
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-18 18:07:57 +0200

    IRepository Interface + CategoryRepository + Replace DbContext with Category Repository

--- IhorsBookWeb/Program.cs
+++ IhorsBookWeb/Program.cs
@@ -1,4 +1,6 @@
 using IhorsBook.DataAccess;
+using IhorsBook.DataAccess.Repository;
+using IhorsBook.DataAccess.Repository.IRepository;
 //using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
