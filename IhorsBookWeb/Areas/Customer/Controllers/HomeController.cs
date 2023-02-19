c2b7c297a165769318a403663444db72e108caea
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-19 20:08:11 +0200

    add items to shopping cart

--- IhorsBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ IhorsBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using IhorsBook.DataAccess.Repository.IRepository;
 using IhorsBook.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace IhorsBookWeb.Controllers;
 [Area("Customer")]
@@ -25,17 +27,31 @@ public class HomeController : Controller
 
         return View(productList);
     }
-    public IActionResult Details(int id)
+    public IActionResult Details(int productId)
     {
         ShoppingCart cartObj = new()
         {
             Count = 1,
-            Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType"),
+            ProductId = productId,
+            Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId, includeProperties: "Category,CoverType"),
         };
 
         return View(cartObj);
     }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize]
+    public IActionResult Details(ShoppingCart shoppingCart)
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        shoppingCart.ApplicationUserId = claim.Value;
 
+        _unitOfWork.ShoppingCart.Add(shoppingCart);
+        _unitOfWork.Save();
+
+        return RedirectToAction(nameof(Index));
+    }
 
     public IActionResult Privacy()
     {
