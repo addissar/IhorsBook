fb0730decb404db8d542aa57155ec4823808ecb8
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-19 16:54:08 +0200

    add Roles for Website

--- IhorsBookWeb/Program.cs
+++ IhorsBookWeb/Program.cs
@@ -3,6 +3,8 @@ using IhorsBook.DataAccess.Repository;
 using IhorsBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using IhorsBook.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +13,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
-builder.Services.AddDefaultIdentity<IdentityUser>() //options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders() //options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
