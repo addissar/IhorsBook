6e827b615c0ba73755a3320853557ba2f68f1bb9
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-19 03:44:47 +0200

    add Update Product

--- IhorsBook.DataAccess/Repository/ProductRepository.cs
+++ IhorsBook.DataAccess/Repository/ProductRepository.cs
@@ -19,7 +19,7 @@ namespace IhorsBook.DataAccess.Repository
         public void Update(Product obj)
         {
             var objFromDb = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
-            if(objFromDb == null)
+            if(objFromDb != null)
             {
                 objFromDb.Title = obj.Title;
                 objFromDb.ISBN = obj.ISBN;
--- IhorsBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ IhorsBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -57,9 +57,11 @@ namespace IhorsBookWeb.Controllers;
         else
         {
             //update product
-        }
+            productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
             return View(productVM);
+
         }
+    }
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -74,13 +76,29 @@ namespace IhorsBookWeb.Controllers;
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
 
+                    if(obj.Product.ImageUrl != null)
+                    {
+                    var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                    }
+                    
                     using(var fileStreams = new FileStream(Path.Combine(uploads,fileName + extension),FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
                     }
                     obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
                 }
-                _unitOfWork.Product.Add(obj.Product);
+                if(obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+            }
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
