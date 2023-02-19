d74962724a0dc8c019ffd2ef1d0eb479427daaf1
Ziubin <ivan.ziubin@cruxlab.com>
2023-02-19 21:45:51 +0200

    add Shopping Cart UI + quantity Price

--- IhorsBook.DataAccess/Repository/Repository.cs
+++ IhorsBook.DataAccess/Repository/Repository.cs
@@ -25,9 +25,14 @@ namespace IhorsBook.DataAccess.Repository
         }
 
         //includeProp - "Category,CoverType"
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter=null, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
             if (includeProperties != null)
             {
                 foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
--- IhorsBook.Models/ShoppingCart.cs
+++ IhorsBook.Models/ShoppingCart.cs
@@ -24,5 +24,8 @@ namespace IhorsBook.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+
+        [NotMapped]
+        public double Price { get; set; }
     }
 }
